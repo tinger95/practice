1c3ebcdad0a9bc42f32f9e40a9ae97f040dbd52f
wutt <ting_net@163.com>
2018-11-21 16:45:14 +0800

    wa

--- src/com/eb/main/Main.java
+++ src/com/eb/main/Main.java
@@ -10,7 +10,5 @@ public class Main {
         RedisClientTemplate redisClient = (RedisClientTemplate)ac.getBean("redisClientTemplate");
         redisClient.set("a", "abc");
         System.out.println(redisClient.get("a"));
-
-
     }
 }
